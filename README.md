c31817702da0153a71240b6b9154d8cdcd051f69
Sergi Calvet <scalvet@profile.es>
2020-06-17 17:08:05 +0200

    moved to root folder

--- README.md
+++
@@ -1,6 +0,0 @@
-# Demo projects
-
-## Build all of them
-```shell script
-./mvn clean package
-```
--- demo-actuator/src/main/java/com/example/demo/actuator/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.actuator.domain.repository;
-
-import com.example.demo.actuator.domain.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
